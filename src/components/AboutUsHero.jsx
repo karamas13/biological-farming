503c8660e555c104bce1cb9ad8c6f49f25e556ea
Νίκος Καραμαρούδης <99655065+karamas13@users.noreply.github.com>
2025-02-10 18:26:15 +0200

    New Loding Screen for aboutus

--- src/components/AboutUsHero.jsx
+++ src/components/AboutUsHero.jsx
@@ -18,9 +18,41 @@ import { FaAngleLeft } from "react-icons/fa";
 
  const AboutUsHero = () => {
   
+ const [loading, setLoading] = useState(true);
+
+
+  // Function to handle video load complete and set minimum loading time
+
+  useEffect(() => {
+    // Automatically hide the loading screen after the minimum time
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500); // Minimum 4.5 seconds delay
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, []);
+
   const nav = useNavigate();
 
   return (
+   <div> 
+     {/* Page Loading Animation */}
+     {loading && 
+        <div className="fixed top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center z-50 bg-stone-950  bg-opacity-100 overscroll-y-contain !scrollbar-none">
+          <section className="grid gap-2 text-lime-400 font-serif py-10 text-center">
+            <FlipText>Loading...</FlipText>
+            <FlipText>Loading...</FlipText>
+            <FlipText>Loading...</FlipText>
+          </section>
+        <div role="status" className="shadow-2xl shadow-lime-500 rounded-full h-fit w-fit">
+            <svg aria-hidden="true" className="w-24 h-24 text-white animate-spin fill-lime-500 " viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ animationDuration: '0.4s'}}>
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+            </svg>           
+        </div>
+
+        </div>
+   
+     }
     <ReactLenis root options={{lerp:0.04}}>
     <div className="bg-zinc-950 font-mono">
       <TextParallaxContent
@@ -48,6 +80,7 @@ import { FaAngleLeft } from "react-icons/fa";
     <Footer />
     <button onClick={()=> {nav("/")}} className=" font-mono flex items-center fixed bottom-0 left-0 text-[#333] m-2 md:m-4 lg:m-6 bg-[#f1f1f1] py-2 px-4 rounded-full opacity-65 text-lg md:text-2xl lg:text-3xl hover:opacity-100 transition ease in out duration-[0.6s]"><FaAngleLeft />Back</button>
     </ReactLenis>
+    </div>
   );
 };
 
@@ -416,5 +449,89 @@ const FAQAccordion = () => {
         </AnimatePresence>
       </div>
     </div>
+  );
+};
+
+const DURATION = 2;
+const STAGGER = 0.095;
+
+const FlipText = ({ children, href }) => {
+  return (
+    <motion.section
+      className="relative block overflow-hidden whitespace-nowrap text-4xl font-black uppercase sm:text-7xl md:text-8xl lg:text-9xl font-sans"
+      href={href}
+      initial="initial"
+      animate="animate" // Set animation state to "animate"
+      variants={{
+        initial: {
+          opacity: 1,
+        },
+        animate: {
+          opacity: 1,
+          transition: {
+            staggerChildren: STAGGER, // Stagger the animation of each letter
+            repeat: Infinity, // Repeat the animation infinitely
+            repeatType: "loop", // Loop the animation
+            duration: 5, // Total duration for one full cycle of animation
+          }
+        }
+      }}
+    >
+      {/* Top Text Animation */}
+      <div>
+        {children.split("").map((l, i) => {
+          return (
+            <motion.span
+              key={i}
+              variants={{
+                initial: {
+                  y: 0,
+                },
+                animate: {
+                  y: "-100%", // Animate upwards out of view
+                },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: STAGGER * i,
+                repeat: Infinity, // Loop the transition
+              }}
+              className="inline-block"
+            >
+              {l}
+            </motion.span>
+          );
+        })}
+      </div>
+
+      {/* Bottom Text Animation */}
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => {
+          return (
+            <motion.span
+              key={i}
+              variants={{
+                initial: {
+                  y: "100%", // Start off-screen at the bottom
+                },
+                animate: {
+                  y: 0, // Move text into position
+                },
+              }}
+              transition={{
+                duration: DURATION,
+                ease: "easeInOut",
+                delay: STAGGER * i,
+                repeat: Infinity, // Loop the transition
+              }}
+              className="inline-block"
+            >
+              {l}
+            </motion.span>
+          );
+        })}
+      </div>
+    </motion.section>
   );
 };
